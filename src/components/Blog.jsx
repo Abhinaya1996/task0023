7a4ef8f7843b3c813c72cfdbe7bcf88397ac1e58
Abhinaya <abhinaya.dev96@gmail.com>
2024-12-20 21:31:27 +0530

    Updated image alt values

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,39 +8,12 @@ export default function Blog(){
         <div className={blogstyles.blogs}>
             <div className={blogstyles.blogsColumn}>
                 <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog1-450x580.jpg.png" alt="Long Image 1" className={blogstyles.long} />
+                    <img src="/assets/blog1-450x580.jpg.png" alt="Blog post image" className={blogstyles.long} />
                     <div className={blogstyles.ribbon}><p>20 April</p></div>
                     <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
                 </div>
                 <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog1-450x580.jpg.png" alt="Short Image 2" className={blogstyles.short} />
-                    <div className={blogstyles.ribbon}><p>20 April</p></div>
-                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
-                </div>
-            </div>
-
-            <div className={blogstyles.blogsColumn}>
-                <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog1-450x580.jpg.png" alt="Short Image 2" className={blogstyles.short} />
-                    <div className={blogstyles.ribbon}><p>20 April</p></div>
-                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
-                </div>
-                <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog2-450x580.jpg.png" alt="Long Image 2" className={blogstyles.long} />
-                    <div className={blogstyles.ribbon}><p>20 April</p></div>
-                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
-                </div>
-            </div>
-
-
-            <div className={blogstyles.blogsColumn}>
-                <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog1-450x580.jpg.png" alt="Long Image 3" className={blogstyles.long}/>
-                    <div className={blogstyles.ribbon}><p>20 April</p></div>
-                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
-                </div>
-                <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog2-450x580.jpg.png" alt="Short Image 3" className={blogstyles.short} />
+                    <img src="/assets/blog1-450x580.jpg.png" alt="Blog post image 2" className={blogstyles.short} />
                     <div className={blogstyles.ribbon}><p>20 April</p></div>
                     <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
                 </div>
@@ -48,12 +21,39 @@ export default function Blog(){
 
             <div className={blogstyles.blogsColumn}>
                 <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog1-450x580.jpg.png" alt="Long Image 3" className={blogstyles.short}/>
+                    <img src="/assets/blog1-450x580.jpg.png" alt="Blog post image 3" className={blogstyles.short} />
                     <div className={blogstyles.ribbon}><p>20 April</p></div>
                     <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
                 </div>
                 <div className={blogstyles.imageWrapper}>
-                    <img src="/assets/blog2-450x580.jpg.png" alt="Short Image 3" className={blogstyles.long} />
+                    <img src="/assets/blog2-450x580.jpg.png" alt="Blog post image 4" className={blogstyles.long} />
+                    <div className={blogstyles.ribbon}><p>20 April</p></div>
+                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
+                </div>
+            </div>
+
+
+            <div className={blogstyles.blogsColumn}>
+                <div className={blogstyles.imageWrapper}>
+                    <img src="/assets/blog1-450x580.jpg.png" alt="Blog post image 5" className={blogstyles.long}/>
+                    <div className={blogstyles.ribbon}><p>20 April</p></div>
+                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
+                </div>
+                <div className={blogstyles.imageWrapper}>
+                    <img src="/assets/blog2-450x580.jpg.png" alt="Blog post image 6" className={blogstyles.short} />
+                    <div className={blogstyles.ribbon}><p>20 April</p></div>
+                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
+                </div>
+            </div>
+
+            <div className={blogstyles.blogsColumn}>
+                <div className={blogstyles.imageWrapper}>
+                    <img src="/assets/blog1-450x580.jpg.png" alt="Blog post image 7" className={blogstyles.short}/>
+                    <div className={blogstyles.ribbon}><p>20 April</p></div>
+                    <div className={blogstyles.imageTitle}>The Covid-19 Epidemic In 2022 <br/> Is Back</div>
+                </div>
+                <div className={blogstyles.imageWrapper}>
+                    <img src="/assets/blog2-450x580.jpg.png" alt="Blog post image 8" className={blogstyles.long} />
                     <div className={blogstyles.ribbon}><p>20 April</p></div>
                     <div className={blogstyles.imageTitle}>Hac hendrerit mus nons semper <br/> suspendisse</div>
                 </div>
